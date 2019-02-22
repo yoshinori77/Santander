06a366459ae251981ecfe90395203a7763ad12b6
yoshinori77 <yoshinoriyamashita123@gmail.com>
2019-02-22 19:15:01 +0900

    雑にモデリング & 評価

--- model.py
+++ model.py
@@ -30,3 +30,20 @@ df.select_dtypes(include='object')
 
 sns.pairplot(df.iloc[:, :10], hue='target', plot_kws={'alpha': 0.2})
 df.groupby('target').describe()
+
+X = df.drop(['target', 'ID_code'], axis=1)
+y = df.target
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = lgb.LGBMClassifier(objective='binary', num_leaves=10, class_weight='balanced',
+                            learning_rate=0.05, n_estimators=5000,
+                            boosting='gbdt', metric='auc')
+
+model.fit(X_train, y_train, eval_set=[(X_val, y_val)],
+            early_stopping_rounds=100, verbose=100)
+y_pred = model.predict(X_val)
+roc_auc_score(y_val, y_pred)
+accuracy_score(y_val, y_pred)
+print(classification_report(y_val, y_pred))
+confusion_matrix(y_val, y_pred)
